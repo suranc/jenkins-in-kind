4f58164e55b9eaa0897c3abe8431ca7d231c4fe6
Chris Suran <suran.c@gmail.com>
2019-09-29 19:21:12 -0400

    Adding loop to poll status of tiller

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-kind cluster create
+# Use kind to create cluster
+kind create cluster
 export KUBECONFIG=$(kind get kubeconfig-path)
+
+# Create malgoof namespace, service account, role and role binding
+kubectl apply -f namespace-service-account-role-rolebinding.yaml
 kubectl config set-context --current --namespace malgoof
 
-kubectl apply -f namespace-service-account-role-rolebinding.yaml
+# Install tiller
 helm init --service-account malgoof --tiller-namespace malgoof
+
+# Check if tiller is ready
+helm version --tiller-namespace malgoof
+while [ $? -ne 0 ]
+do
+    sleep 5
+    helm version --tiller-namespace malgoof
+done
+
+# Install Jenkins into malgoof namespace
 helm install -n jenkins stable/jenkins -f values.yaml  --tiller-namespace malgoof --namespace malgoof
